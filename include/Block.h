ae6207caf2200f9e4240dd211da2ee8f4eda5eb9
Danno19 <47689119+Danno19@users.noreply.github.com>
2020-03-29 17:25:56 +0300

    Add files via upload

--- include/Block.h
+++ include/Block.h
@@ -11,12 +11,46 @@
 class Block
 {
 private:
-    std::array<Word, BLOCK_LENGTH> words;
+    Word words[BLOCK_LENGTH];
 public:
-    Block();
-    Word get_word(unsigned int);
-    bool set_word(unsigned int, Word);
-    ~Block();
+    Block()
+    {
+        //words[i].set_int(0);
+    }
+
+    Word get_word(unsigned int position)
+    {
+        if ((position >= 0) && (position < BLOCK_LENGTH))
+        {
+            return words[position];
+        }
+        throw "Bad block position";
+    }
+
+    bool set_word(unsigned int position, Word word)
+    {
+        if ((position >= 0) && (position < BLOCK_LENGTH))
+        {
+            words[position] = word;
+            return true;
+        }
+        return false;
+    }
+
+    bool set_int(unsigned int position, unsigned int value)
+    {
+        if ((position >= 0) && (position < BLOCK_LENGTH))
+        {
+            words[position].set_int(value);
+            return true;
+        }
+        return false;
+    }
+
+    ~Block()
+    {
+        std::fill(words, BLOCK_LENGTH, 0);
+    }
 };
 
 #endif
