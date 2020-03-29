71a1173ae36c27a40f1ac3d1a5dfb78e1dfd8ace
Liutauras Gaidamavičius <liutauras@unity3d.com>
2020-03-29 16:45:26 +0300

    Created methods for word.h

--- include/word.h
+++ include/word.h
@@ -1,17 +1,40 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include <stdio.h>
+
+#define WORD_LENGTH 4
 
 struct Word{
 private:
-    uint8_t bytes[4];
+    uint8_t bytes[WORD_LENGTH];
 public:
-    int to_int(){
-
+    Word(){}
+    
+    uint8_t get_byte(int index){
+        return bytes[index];
     }
 
-    std::string to_string()
-    {
+    void set_byte(int index, uint8_t value){
+        bytes[index] = value;
+    }
 
+    int get_int()
+    {
+        return (int)bytes[0] << 24 |
+                    bytes[1] << 16 |
+                    bytes[2] << 8  |
+                    bytes[3];
+    }
+
+    void set_int(int value){
+        bytes[0] = (value >> 24) & 0xFF;
+        bytes[1] = (value >> 16) & 0xFF;
+        bytes[2] = (value >> 8) & 0xFF;
+        bytes[3] = value & 0xFF;
+    }
+
+    void print_bytes(){
+        printf("Bytes: %X %X %X %X\n", bytes[0], bytes[1], bytes[2], bytes[3]);
     }
 };
