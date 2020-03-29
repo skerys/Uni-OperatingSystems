825bed80a824eca5386e06d74ecba0c8415f2b91
Danno19 <47689119+Danno19@users.noreply.github.com>
2020-03-29 15:15:37 +0300

    Add files via upload

---
+++ include/Memory.h
@@ -0,0 +1,20 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include "VM.h"
+
+class Memory
+{
+private:
+
+public:
+    Memory();
+    ~Memory();
+    void print();
+};
+
+#endif
