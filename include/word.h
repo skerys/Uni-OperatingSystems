4449a4f9e0c218f0304c6093a722d3b29b7552de
Liutauras Gaidamavičius <liutauras@unity3d.com>
2020-03-29 16:16:44 +0300

    Added word.h

---
+++ include/word.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <cstdint>
+#include <string>
+
+struct Word{
+private:
+    uint8_t bytes[4];
+public:
+    int to_int(){
+
+    }
+
+    std::string to_string()
+    {
+
+    }
+};
