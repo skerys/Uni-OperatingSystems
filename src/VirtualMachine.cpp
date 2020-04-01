e83fd35efa35b1af0b613362397e541f77a86133
Danno19 <danfemas@gmail.com>
2020-04-01 20:47:12 +0300

    VM pirmosios komandos

--- src/VirtualMachine.cpp
+++ src/VirtualMachine.cpp
@@ -2,7 +2,7 @@
 
 VirtualMachine::VirtualMachine()
 {
-    this->running = true;
+    running = true;
 }
 
 void VirtualMachine::execute()
@@ -23,6 +23,69 @@ void VirtualMachine::loadMemory(Memory _memory)
 void VirtualMachine::run()
 {
 
+}
+
+//Write from memory to register: WAxy WBxy
+void VirtualMachine::writeToMemory(RegisterType registerType, int memoryAddress)
+{
+    switch(registerType)
+    {
+        case RegisterType::RA:
+            memory[memoryAddress/16][memoryAddress%16] = ra.get_value();
+            break;
+        case RegisterType::RB:
+            memory[memoryAddress/16][memoryAddress%16] = rb.get_value();
+            break;
+        default:
+            break;
+    }
+}
+
+//Load from memory to register: LAxy LBxy
+void VirtualMachine::loadFromMemory(RegisterType registerType, int memoryAddress)
+{
+    switch(registerType)
+    {
+        case RegisterType::RA:
+            ra.set_value(memory[memoryAddress/16][memoryAddress%16].get_int());
+            break;
+        case RegisterType::RB:
+            rb.set_value(memory[memoryAddress/16][memoryAddress%16].get_int());
+            break;
+        default:
+            break;
+    }
+}
+
+void VirtualMachine::arithmethicCommand(ArithmeticCommand arithmeticCommand)
+{
+    switch(arithmeticCommand)
+    {
+        case ArithmeticCommand::ADD:
+            ra.set_value(ra.get_value() + rb.get_value());
+            break;
+        case ArithmeticCommand::SUB:
+            ra.set_value(ra.get_value() - rb.get_value());
+            break;
+        case ArithmeticCommand::MUL:
+            ra.set_value(ra.get_value() * rb.get_value());
+            break;
+        case ArithmeticCommand::DIV:
+            ra.set_value(ra.get_value() / rb.get_value());
+            break;
+        default:
+            break;
+    }
+}
+
+void VirtualMachine::compareRegisters()
+{
+    if (ra.get_value() == rb.get_value())
+        sf.set_status(0);
+    else if (ra.get_value() > rb.get_value())
+        sf.set_status(1);
+    else
+        sf.set_status(2);    
 }
 
 VirtualMachine::~VirtualMachine()
